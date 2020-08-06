7dcbaadc4bc4833df97dbc31f8e4c273c0d58835
wfparrish <williparrish@gmail.com>
2020-08-06 14:36:17 -0600

    open close modal functionality added

--- shared.js
+++ shared.js
@@ -1,12 +1,19 @@
 let backdrop = document.querySelector('.backdrop');
 let modal = document.querySelector('.modal');
-
 let selectPlanButtons = document.querySelectorAll('.plan button');
-console.log(selectPlanButtons)
+let modalNoButton = document.querySelector('.modal__action--negative');
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener('click', function() {
     modal.style.display = 'block';
     backdrop.style.display = 'block';
   });
+}
+
+backdrop.addEventListener("click", closeModal);
+modalNoButton.addEventListener("click", closeModal);
+
+function closeModal() {
+  modal.style.display = 'none';
+  backdrop.style.display = 'none';
 }
